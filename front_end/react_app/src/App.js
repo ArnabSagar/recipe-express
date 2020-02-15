77ce3cbcda5f99c547b0bf59c1af21802491b89d
Arnab Sagar <arnab.sagar47@gmail.com>
2020-02-15 10:56:39 -0500

    Added code

--- front_end/react_app/src/App.js
+++ front_end/react_app/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          RecipeExpress Website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    todos:[
+      {
+        id:1,
+        title: "diiner",
+        completed: false
+      },
+      {
+        id:2,
+        title: "trash",
+        completed: false
+      },
+      {
+        id:3,
+        title: "dance",
+        completed: false
+      }
+    ]
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <Todos todos = {this.state.todos}/>
+
+            <div id="myDIV" class="header">
+            <h2>My To Do List</h2>
+            <input type="text" id="myInput" placeholder="Title..."></input>
+            <span onclick="newElement()" class="addBtn">Add</span>
+            </div>
+
+          <ul id="myUL">
+            <li>Hit the gym</li>
+            <li class="checked">Pay bills</li>
+            <li>Meet George</li>
+            <li>Buy eggs</li>
+            <li>Read a book</li>
+            <li>Organize office</li>
+          </ul>
+
+      </div>
+
+    );
+  }
 }
 
 export default App;
