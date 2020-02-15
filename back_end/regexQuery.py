406109a677b7bad9d75a24dd665c97f2f9831a3b
Matt Anderson <anderm28@mcmaster.ca>
2020-02-15 17:28:21 -0500

    Completed regex query module

--- back_end/regexQuery.py
+++ back_end/regexQuery.py
@@ -1,10 +1,9 @@
 import pymongo
 import json
 import pprint
-import re
 
 
-def searchRecipes():
+def searchRecipes(ingredients):
     # connecting to the Atlas cluster
     client = pymongo.MongoClient("mongodb+srv://npysklyw:Maer1234@cluster0-g1qmd.gcp.mongodb.net/test?retryWrites=true&w=majority")
 
@@ -12,10 +11,22 @@ def searchRecipes():
     db = client.RecipeOther
     Recipes = db.Recipe
 
-    hits = Recipes.find({"Ingredients":{'$regex':'/.*chicken.*/'}})
+    # conditional calls depending on number of input ingredients
+    if (len(ingredients) == 1):
+        regex1 = "^.*" + ingredients[0]
+        hits = Recipes.find({"Ingredients":{'$regex':regex1}})
+    elif (len(ingredients) == 2):
+        regex1 = "^.*" + ingredients[0]
+        regex2 = "^.*" + ingredients[1] 
+        hits = Recipes.find({"Ingredients":{'$regex':regex1}, "Ingredients":{'$regex':regex2}})
+    else:
+        regex1 = "^.*" + ingredients[0]
+        regex2 = "^.*" + ingredients[1] 
+        regex3 = "^.*" + ingredients[2]
+        hits = Recipes.find({"Ingredients":{'$regex':regex1}, "Ingredients":{'$regex':regex2}, "Ingredients":{'$regex':regex3}})
+
 
     for hit in hits:
         pprint.pprint(hit)
 
-
-def searchRecipes
+searchRecipes(["chicken", "ground pepper", "herbs"])
