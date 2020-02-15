17b2cc388443a251759f63b5fcb1364def7bb732
Matt Anderson <anderm28@mcmaster.ca>
2020-02-15 16:40:28 -0500

    Added functions to push alternative dataset to database

--- back_end/old/ripRecipes.py
+++ back_end/old/ripRecipes.py
@@ -22,7 +22,7 @@ count = 0
 for key in keywords:
     count += 1
     if(count % 4 == 0):
-        time.sleep(30)
+        time.sleep(45)
     FULL_URL = BASE_URL + "?q=" + key + "&app_id=" + APP_ID + "&app_key=" + APP_KEY
     response = requests.get(url=FULL_URL)
     if not response.ok:
